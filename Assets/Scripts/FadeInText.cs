b72c3af94f6a8df8d63b64d498c1507e83c5af42
Hanus Valenta <hanus.valenta@gmail.com>
2025-04-11 00:04:43 +0200

    new fonts and one new script

---
+++ Assets/Scripts/FadeInText.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FadeInTMPText : MonoBehaviour
+{
+    public float delayBeforeFade = 0.5f;
+    public float fadeDuration = 2f;
+
+    private TextMeshProUGUI tmpText;
+
+    void Awake()
+    {
+        tmpText = GetComponent<TextMeshProUGUI>();
+        SetTextAlpha(0f);
+    }
+
+    void Start()
+    {
+        StartCoroutine(FadeInText());
+    }
+
+    IEnumerator FadeInText()
+    {
+        yield return new WaitForSeconds(delayBeforeFade);
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            float alpha = Mathf.Clamp01(elapsed / fadeDuration);
+            SetTextAlpha(alpha);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        SetTextAlpha(1f);
+    }
+
+    void SetTextAlpha(float alpha)
+    {
+        Color c = tmpText.color;
+        c.a = alpha;
+        tmpText.color = c;
+    }
+}
