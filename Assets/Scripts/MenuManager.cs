60434960de5ec1037effb2b36dce61f8da4ead3a
Hanus Valenta <hanus.valenta@gmail.com>
2025-04-23 00:53:09 +0200

    broken shader

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,6 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public AudioClip MenuMusic;
+    public float MusicVolume = 0.5f;
+
+    private void Start()
+    {
+        if (MenuMusic != null)
+        {
+            AudioSource MenuMusicSource = gameObject.AddComponent<AudioSource>();
+            MenuMusicSource.clip = MenuMusic;
+            MenuMusicSource.loop = true;
+            MenuMusicSource.volume = MusicVolume;
+            MenuMusicSource.Play();
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
@@ -10,7 +25,6 @@ public class MainMenuManager : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("F*ck this game, I'm out!");
         Application.Quit();
     }
 }
