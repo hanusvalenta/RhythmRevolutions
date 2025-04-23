7ca54adaf492f873be67711c22371b182707dbcb
Hanus Valenta <hanus.valenta@gmail.com>
2025-04-23 23:25:34 +0200

    cool lines

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,38 +1,81 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+ using UnityEngine.SceneManagement;
 
-public class Projectile : MonoBehaviour
-{
-    public float speed = 5f;
+ public class Projectile : MonoBehaviour
+ {
+  public float speed = 5f;
+  private LineRenderer holdIndicatorLine;
+  private float holdDuration;
+  private float holdTimer = 0f;
+  private bool isHoldNote = false;
+  private Vector3 startPosition;
 
-    void Update()
+  void Start()
+  {
+   startPosition = transform.position;
+  }
+
+  public void InitializeHold(float duration)
+  {
+   isHoldNote = true;
+   holdDuration = duration;
+   holdIndicatorLine = gameObject.AddComponent<LineRenderer>();
+   holdIndicatorLine.positionCount = 2;
+   holdIndicatorLine.startWidth = 0.1f;
+   holdIndicatorLine.endWidth = 0.1f;
+   holdIndicatorLine.startColor = Color.white;
+   holdIndicatorLine.endColor = Color.white;
+   holdIndicatorLine.useWorldSpace = true;
+
+   // Create a simple white material in code
+   Material lineMaterial = new Material(Shader.Find("Unlit/Color")); // Use Unlit/Color shader
+   lineMaterial.color = Color.white;
+   holdIndicatorLine.material = lineMaterial;
+  }
+
+  void Update()
+  {
+   transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+   if (transform.position.x < -10f)
+   {
+    LoadDeathScene();
+   }
+
+   if (isHoldNote)
+   {
+    UpdateHoldIndicator();
+   }
+  }
+
+  void UpdateHoldIndicator()
+  {
+   holdTimer += Time.deltaTime;
+   float lineLength = holdDuration * speed;
+   Vector3 endPosition = startPosition + Vector3.down * Mathf.Clamp01(holdTimer / holdDuration) * lineLength;
+
+   holdIndicatorLine.SetPosition(0, startPosition);
+   holdIndicatorLine.SetPosition(1, endPosition);
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+   if (other.CompareTag("Target"))
+   {
+    Target target = other.GetComponent<Target>();
+    if (target != null && target.IsActive)
     {
-        transform.Translate(Vector2.down * speed * Time.deltaTime);
-
-        if (transform.position.x < -10f)
-        {
-            LoadDeathScene();
-        }
+     Destroy(gameObject);
     }
-
-    void OnTriggerEnter2D(Collider2D other)
+    else
     {
-        if (other.CompareTag("Target"))
-        {
-            Target target = other.GetComponent<Target>();
-            if (target != null && target.IsActive)
-            {
-                Destroy(gameObject);
-            }
-            else
-            {
-                LoadDeathScene();
-            }
-        }
+     LoadDeathScene();
     }
+   }
+  }
 
-    void LoadDeathScene()
-    {
-        SceneManager.LoadScene("DeathScene");
-    }
-}
+  void LoadDeathScene()
+  {
+   SceneManager.LoadScene("DeathScene");
+  }
+ }
--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -2,79 +2,112 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 
 public class SpawnerController : MonoBehaviour
 {
-    public GameObject projectilePrefab;
-    public Transform[] spawners;
-    public SpawnPattern spawnPattern;
-    public Button[] buttonsToUnhide = new Button[2];
-    public GameObject[] elementsToHide;
+ public GameObject projectilePrefab;
+ public Transform[] spawners;
+ public SpawnPattern spawnPattern;
+ public Button[] buttonsToUnhide = new Button[2];
+ public GameObject[] elementsToHide;
 
-    private float elapsedTime = 0f;
-    private int nextSpawnIndex = 0;
-    private bool hasSpawnedAll = false;
-    private float timeSinceLastSpawn = 0f;
-    private const float delayBeforeUnhide = 5f;
+ private float elapsedTime = 0f;
+ private int nextSpawnIndex = 0;
+ private bool hasSpawnedAll = false;
+ private float timeSinceLastSpawn = 0f;
+ private const float delayBeforeUnhide = 5f;
 
-    void Start()
+ private GameObject currentProjectile;
+ private float holdStartTime;
+ private float holdEndTime;
+ private bool isHolding = false;
+
+ void Start()
+ {
+  buttonsToUnhide[0].gameObject.SetActive(false);
+  buttonsToUnhide[1].gameObject.SetActive(false);
+ }
+
+ void Update()
+ {
+  elapsedTime += Time.deltaTime;
+
+  if (!hasSpawnedAll)
+  {
+   while (nextSpawnIndex < spawnPattern.spawnData.Length &&
+          spawnPattern.spawnData[nextSpawnIndex].time <= elapsedTime)
+   {
+    SpawnProjectile(spawnPattern.spawnData[nextSpawnIndex]);
+    nextSpawnIndex++;
+    timeSinceLastSpawn = 0f;
+   }
+
+   if (nextSpawnIndex >= spawnPattern.spawnData.Length)
+   {
+    hasSpawnedAll = true;
+   }
+  }
+  else
+  {
+   timeSinceLastSpawn += Time.deltaTime;
+
+   if (timeSinceLastSpawn >= delayBeforeUnhide)
+   {
+    UnhideButtonsAndHideElements();
+   }
+  }
+
+  if (isHolding)
+  {
+   if (elapsedTime >= holdEndTime)
+   {
+    EndHold();
+   }
+  }
+ }
+
+ void SpawnProjectile(SpawnData data)
+ {
+  if (data.spawnerIndex >= 0 && data.spawnerIndex < spawners.Length)
+  {
+   currentProjectile = Instantiate(projectilePrefab, spawners[data.spawnerIndex].position, Quaternion.identity);
+
+   if (data.holdNote)
+   {
+    isHolding = true;
+    holdStartTime = elapsedTime;
+    holdEndTime = elapsedTime + data.holdDuration;
+    Projectile projectileComponent = currentProjectile.GetComponent<Projectile>();
+    if (projectileComponent != null)
     {
-        buttonsToUnhide[0].gameObject.SetActive(false);
-        buttonsToUnhide[1].gameObject.SetActive(false);
+     projectileComponent.InitializeHold(data.holdDuration);
     }
+   }
+  }
+  else
+  {
+   Debug.LogWarning("Invalid spawner index: " + data.spawnerIndex);
+  }
+ }
 
-    void Update()
-    {
-        elapsedTime += Time.deltaTime;
+ void EndHold()
+ {
+  isHolding = false;
+  currentProjectile = null;
+  Debug.Log("Hold note ended!");
+ }
 
-        if (!hasSpawnedAll)
-        {
-            while (nextSpawnIndex < spawnPattern.spawnData.Length &&
-                   spawnPattern.spawnData[nextSpawnIndex].time <= elapsedTime)
-            {
-                SpawnProjectile(spawnPattern.spawnData[nextSpawnIndex]);
-                nextSpawnIndex++;
-                timeSinceLastSpawn = 0f;
-            }
+ void UnhideButtonsAndHideElements()
+ {
+  buttonsToUnhide[0].gameObject.SetActive(true);
+  buttonsToUnhide[1].gameObject.SetActive(true);
 
-            if (nextSpawnIndex >= spawnPattern.spawnData.Length)
-            {
-                hasSpawnedAll = true;
-            }
-        }
-        else
-        {
-            timeSinceLastSpawn += Time.deltaTime;
+  foreach (var element in elementsToHide)
+  {
+   element.SetActive(false);
+  }
 
-            if (timeSinceLastSpawn >= delayBeforeUnhide)
-            {
-                UnhideButtonsAndHideElements();
-            }
-        }
-    }
-
-    void SpawnProjectile(SpawnData data)
-    {
-        if (data.spawnerIndex >= 0 && data.spawnerIndex < spawners.Length)
-        {
-            Instantiate(projectilePrefab, spawners[data.spawnerIndex].position, Quaternion.identity);
-        }
-        else
-        {
-            Debug.LogWarning("Invalid spawner index: " + data.spawnerIndex);
-        }
-    }
-
-    void UnhideButtonsAndHideElements()
-    {
-        buttonsToUnhide[0].gameObject.SetActive(true);
-        buttonsToUnhide[1].gameObject.SetActive(true);
-
-        foreach (var element in elementsToHide)
-        {
-            element.SetActive(false);
-        }
-
-        this.enabled = false;
-    }
+  this.enabled = false;
+ }
 }
