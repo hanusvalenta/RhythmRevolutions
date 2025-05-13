843c753d64f8cc298bb2a536f4ae92cda223ab05
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-13 22:16:27 +0200

    format

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,93 +1,93 @@
 using UnityEngine;
- using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
- public class Projectile : MonoBehaviour
- {
-  public float speed = 5f;
-  private LineRenderer holdIndicatorLine;
-  private float holdDuration;
-  private float holdTimer = 0f;
-  private bool isHoldNote = false;
-  private Vector3 endPosition;
+public class Projectile : MonoBehaviour
+{
+    public float speed = 5f;
+    private LineRenderer holdIndicatorLine;
+    private float holdDuration;
+    private float holdTimer = 0f;
+    private bool isHoldNote = false;
+    private Vector3 endPosition;
 
-  void Start()
-  {
-   endPosition = transform.position;
-  }
-
-  public void InitializeHold(float duration)
-  {
-   isHoldNote = true;
-   holdDuration = duration;
-   holdIndicatorLine = gameObject.AddComponent<LineRenderer>();
-   holdIndicatorLine.positionCount = 2;
-   holdIndicatorLine.startWidth = 0.1f;
-   holdIndicatorLine.endWidth = 0.1f;
-   holdIndicatorLine.startColor = Color.white;
-   holdIndicatorLine.endColor = Color.white;
-   holdIndicatorLine.useWorldSpace = true;
-
-   Material lineMaterial = new Material(Shader.Find("Unlit/Color"));
-   lineMaterial.color = Color.white;
-   holdIndicatorLine.material = lineMaterial;
-  }
-
-  void Update()
-  {
-   transform.Translate(Vector2.down * speed * Time.deltaTime);
-
-   if (transform.position.x < -10f)
-   {
-    LoadDeathScene();
-   }
-
-   if (isHoldNote)
-   {
-    UpdateHoldIndicator();
-   }
-
-   if (Input.GetKeyDown(KeyCode.Space) && isHoldNote)
-   {
-    HoldSuccessful();
-   }
-  }
-
-  void UpdateHoldIndicator()
-  {
-   holdTimer += Time.deltaTime;
-   float lineLength = holdDuration * speed;
-   Vector3 startPosition = endPosition + Vector3.up * Mathf.Clamp01(holdTimer / holdDuration) * lineLength;
-
-   holdIndicatorLine.SetPosition(0, startPosition);
-   holdIndicatorLine.SetPosition(1, endPosition);
-
-   endPosition = transform.position;
-  }
-
-  void HoldSuccessful()
-  {
-   holdIndicatorLine.enabled = false;
-   isHoldNote = false;
-  }
-
-  void OnTriggerEnter2D(Collider2D other)
-  {
-   if (other.CompareTag("Target"))
-   {
-    Target target = other.GetComponent<Target>();
-    if (target != null && target.IsActive)
+    void Start()
     {
-     Destroy(gameObject);
+        endPosition = transform.position;
     }
-    else
-    {
-     LoadDeathScene();
-    }
-   }
-  }
 
-  void LoadDeathScene()
-  {
-   SceneManager.LoadScene("DeathScene");
-  }
- }
+    public void InitializeHold(float duration)
+    {
+        isHoldNote = true;
+        holdDuration = duration;
+        holdIndicatorLine = gameObject.AddComponent<LineRenderer>();
+        holdIndicatorLine.positionCount = 2;
+        holdIndicatorLine.startWidth = 0.1f;
+        holdIndicatorLine.endWidth = 0.1f;
+        holdIndicatorLine.startColor = Color.white;
+        holdIndicatorLine.endColor = Color.white;
+        holdIndicatorLine.useWorldSpace = true;
+
+        Material lineMaterial = new Material(Shader.Find("Unlit/Color"));
+        lineMaterial.color = Color.white;
+        holdIndicatorLine.material = lineMaterial;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        if (transform.position.x < -10f)
+        {
+            LoadDeathScene();
+        }
+
+        if (isHoldNote)
+        {
+            UpdateHoldIndicator();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isHoldNote)
+        {
+            HoldSuccessful();
+        }
+    }
+
+    void UpdateHoldIndicator()
+    {
+        holdTimer += Time.deltaTime;
+        float lineLength = holdDuration * speed;
+        Vector3 startPosition = endPosition + Vector3.up * Mathf.Clamp01(holdTimer / holdDuration) * lineLength;
+
+        holdIndicatorLine.SetPosition(0, startPosition);
+        holdIndicatorLine.SetPosition(1, endPosition);
+
+        endPosition = transform.position;
+    }
+
+    void HoldSuccessful()
+    {
+        holdIndicatorLine.enabled = false;
+        isHoldNote = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Target"))
+        {
+            Target target = other.GetComponent<Target>();
+            if (target != null && target.IsActive)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                LoadDeathScene();
+            }
+        }
+    }
+
+    void LoadDeathScene()
+    {
+        SceneManager.LoadScene("DeathScene");
+    }
+}
