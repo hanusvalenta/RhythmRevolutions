a10272c7fac3cfc469e1f247bf9e1d775b16a348
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-14 22:47:58 +0200

    fade

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+
+    public Animator fadeAnimator;
+
+    public string fadeInTrigger = "Start";
+    public string sceneToLoadName = "Game";
+
+    public float fadeTime = 1f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(PerformFade(sceneToLoadName));
+    }
+
+    IEnumerator PerformFade(string sceneToLoadName)
+    {
+        fadeAnimator.SetTrigger(fadeInTrigger);
+
+        yield return new WaitForSeconds(fadeTime);
+
+        SceneManager.LoadScene(sceneToLoadName);
+    }
+}
