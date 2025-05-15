8e419d0cae47a0e216f4abf199fe195594180595
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-15 09:56:10 +0200

    spike playerShake

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Spike : MonoBehaviour
 {
-    
     public AudioClip playerDieSound;
+
+    private SpriteRenderer playerSpriteRenderer;
+    private Vector3 originalPosition;
+    private float shakeDuration;
+    private float shakeIntensity;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            playerSpriteRenderer = other.GetComponent<SpriteRenderer>();
+            originalPosition = other.transform.position;
+            shakeDuration = 0.3f;
+            shakeIntensity = 0.1f;
+            StartCoroutine(Shake());
+
             GameObject fadeObject = GameObject.Find("FadeAndLoader");
 
             if (playerDieSound != null)
@@ -26,5 +37,22 @@ public class Spike : MonoBehaviour
                 }
             }
         }
+    }
+
+    IEnumerator Shake()
+    {
+        float elapsed = 0f;
+        while (elapsed < shakeDuration)
+        {
+            float x = Random.Range(-1f, 1f) * shakeIntensity;
+            float y = Random.Range(-1f, 1f) * shakeIntensity;
+
+            playerSpriteRenderer.transform.position = originalPosition + new Vector3(x, y, 0);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        playerSpriteRenderer.transform.position = originalPosition;
     }
 }
