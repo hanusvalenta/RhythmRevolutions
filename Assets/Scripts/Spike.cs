b915a05fd6970ffa7a6806daa5c575c4ba99dd5d
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-15 09:49:27 +0200

    spike sounds

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
- using UnityEngine.SceneManagement;
- 
- public class Spike : MonoBehaviour
- {
-  void OnTriggerEnter2D(Collider2D other)
-  {
-  if (other.CompareTag("Player"))
-  {
-  SceneManager.LoadScene("DeathScene");
-  }
-  }
- }
+using UnityEngine.SceneManagement;
+
+public class Spike : MonoBehaviour
+{
+    
+    public AudioClip playerDieSound;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject fadeObject = GameObject.Find("FadeAndLoader");
+
+            if (playerDieSound != null)
+            {
+                AudioSource.PlayClipAtPoint(playerDieSound, Camera.main.transform.position);
+            }
+
+            if (fadeObject != null)
+            {
+                Fade fade = fadeObject.GetComponent<Fade>();
+                if (fade != null)
+                {
+                    fade.FadeIn("DeathScene");
+                }
+            }
+        }
+    }
+}
