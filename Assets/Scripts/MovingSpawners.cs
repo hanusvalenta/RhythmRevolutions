8ef0e85874769573c7c59fb127c56c5c88f9f1e1
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-21 19:00:28 +0200

    working on map

---
+++ Assets/Scripts/MovingSpawners.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSpawners : MonoBehaviour
+{
+    public Transform targetObject;
+    public float rotationSpeed = 30f;
+    public float radius = 5f;
+    private float angle = 0f;
+
+    void Start()
+    {
+        if (targetObject == null)
+        {
+            enabled = false;
+            return;
+        }
+    }
+
+    void Update()
+    {
+        if (targetObject == null)
+        {
+            return;
+        }
+
+        angle += rotationSpeed * Time.deltaTime;
+
+        if (angle >= 360f)
+        {
+            angle -= 360f;
+        }
+
+        float x = targetObject.position.x + Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
+        float z = targetObject.position.z + Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
+        float y = transform.position.y;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
