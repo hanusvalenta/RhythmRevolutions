719d85b092e88066248efe27b6043f8a4868c213
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-23 00:39:19 +0200

    Boss Barrier

---
+++ Assets/Scripts/BossBarrier.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossBarrier : MonoBehaviour
+{
+    public string barrierText = "You can't pass here yet! Defeat or spare the boss first!";
+    public float textDisplayDuration = 2f;
+    public string bossNameToCheck;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                bool bossDefeatedOrSpared = false;
+                if (GameManager.Instance != null && !string.IsNullOrEmpty(bossNameToCheck))
+                {
+                    var sparedField = typeof(GameManager).GetField(bossNameToCheck + "Spared");
+                    var foughtField = typeof(GameManager).GetField(bossNameToCheck + "Fought");
+
+                    if (sparedField != null && (bool)sparedField.GetValue(GameManager.Instance))
+                    {
+                        bossDefeatedOrSpared = true;
+                    }
+
+                    if (foughtField != null && (bool)foughtField.GetValue(GameManager.Instance))
+                    {
+                        bossDefeatedOrSpared = true;
+                    }
+                }
+
+                if (bossDefeatedOrSpared)
+                {
+                    Destroy(gameObject);
+                    return;
+                }
+                StartCoroutine(HandleBarrierInteraction(player));
+            }
+        }
+    }
+
+    IEnumerator HandleBarrierInteraction(Player player)
+    {
+        if (player.textBox != null)
+        {
+            player.textBox.ShowText(barrierText);
+        }
+        
+        yield return new WaitForSeconds(textDisplayDuration);
+
+        player.MoveToNearestRespawnPoint();
+    }
+}
