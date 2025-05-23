6659fbee35d05615561f71755dabf7b425c290c1
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-23 09:36:58 +0200

    changes?

--- Assets/Scripts/BossFight.cs
+++ Assets/Scripts/BossFight.cs
@@ -43,6 +43,7 @@ public class BossFight : MonoBehaviour
             audioSource.volume = musicVolume;
             if (!audioSource.isPlaying && fightStarted)
             {
+                audioSource.Play();
             }
         }
     }
