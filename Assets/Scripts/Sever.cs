2629cb41bd47623c290e0c2b9af87e9cf8f1f983
Hanus Valenta <hanus.valenta@gmail.com>
2025-05-23 13:36:45 +0200

    Sever Master

--- Assets/Scripts/Sever.cs
+++ Assets/Scripts/Sever.cs
@@ -6,9 +6,11 @@ public class Sever : MonoBehaviour
     public bool isStateTwoTriggered = false;
     public int collisionCount = 0;
 
+    private SpriteRenderer spriteRenderer;
+
     void Awake()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -21,11 +23,13 @@ public class Sever : MonoBehaviour
             {
                 isStateOneTriggered = true;
                 isStateTwoTriggered = false;
+                DimSpriteStateOne();
             }
             else if (collisionCount >= 2)
             {
                 isStateOneTriggered = false;
                 isStateTwoTriggered = true;
+                DimSpriteStateTwo();
             }
         }
     }
@@ -35,5 +39,30 @@ public class Sever : MonoBehaviour
         collisionCount = 0;
         isStateOneTriggered = false;
         isStateTwoTriggered = false;
+        ResetSpriteDimming();
+    }
+
+    private void DimSpriteStateOne()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = new Color(0.8f, 0.8f, 0.8f, 1f);
+        }
+    }
+
+    private void DimSpriteStateTwo()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+        }
+    }
+
+    private void ResetSpriteDimming()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+        }
     }
 }
---
+++ Assets/Scripts/SewerMaster.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SewerMaster : MonoBehaviour
+{
+    public List<Sever> severs;
+    public TextBox textBox;
+
+    private GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    void Update()
+    {
+        CheckAllSeversInStateOne();
+    }
+
+    public void ResetAllSevers()
+    {
+        foreach (Sever sever in severs)
+        {
+            sever.ResetStates();
+        }
+    }
+
+    private void CheckAllSeversInStateOne()
+    {
+        bool allInStateOne = true;
+        foreach (Sever sever in severs)
+        {
+            if (!sever.isStateOneTriggered)
+            {
+                allInStateOne = false;
+                break;
+            }
+        }
+
+        if (allInStateOne && gameManager != null && !gameManager.hasKey1)
+        {
+            gameManager.hasKey1 = true;
+            if (textBox != null)
+            {
+                textBox.ShowText("You got a key!");
+            }
+            this.enabled = false;
+        }
+    }
+}
